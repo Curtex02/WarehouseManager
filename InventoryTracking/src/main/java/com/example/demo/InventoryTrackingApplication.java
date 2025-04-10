63e662faa8857124d4aa868c4e479e9b922488c1
= <=>
2025-04-10 10:10:32 -0400

    added inv tracking

---
+++ InventoryTracking/src/main/java/com/example/demo/InventoryTrackingApplication.java
@@ -0,0 +1,17 @@
+
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+//@EnableDiscoveryClient wasn't working in development may need fix
+@SpringBootApplication
+public class InventoryTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryTrackingApplication.class, args);
+	}
+
+}
